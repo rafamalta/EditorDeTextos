9f9e73b6eed6989b968aa0d8a71f3dd6608b4741
Rafael Malta <rafa_rsbr@hotmail.com>
2022-12-17 20:21:28 -0300

    Iniciando o projeto

---
+++ EditorDeTextos/EditorDeTextos/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace EditorDeTextos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");            
+            Console.WriteLine("1 - Abrir arquivo");            
+            Console.WriteLine("2 - Criar arquivo");            
+            Console.WriteLine("0 - Sair");
+            short opcao = short.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 0: Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Criar();break;
+                default: Menu(); break;
+            }            
+        }
+
+        static void Abrir()
+        {
+
+        }
+
+        static void Criar()
+        {
+
+        }       
+    }
+}
